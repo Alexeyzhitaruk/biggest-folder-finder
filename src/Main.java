3f175d3d6d8641ca66268ad2c115e27d86f18c35
Alexey <alexey.wse660@gmail.com>
2023-07-18 19:08:59 +0600

    add class Node, class SizeCalculator, class FolderSizeCalculator

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "C:\\Users\\Alexey\\Desktop";
-        File file = new File(folderPath);
+        File file = new File("C:\\Users\\Alexey\\Desktop");
+        Node root = new Node(file);
 
-        System.out.println(getFolderSize(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(calculator);
+
+        System.out.println(root);
+
     }
-
-     public static long getFolderSize(File folder){
-        if(folder.isFile()){
-            return folder.length();
-        }
-        long sum = 0;
-        for(File file : folder.listFiles()){
-            sum += getFolderSize(file);
-        }
-        return  sum;
-     }
 }
